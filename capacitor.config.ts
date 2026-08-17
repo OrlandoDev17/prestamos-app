1d41686c85caa59c21110c00caa7b1169f34d5f3
OrlandoDev <orlandodev1707@gmail.com>
2026-08-17 15:15:10 -0400

    🔧 chore: actualizar webDir de Capacitor y archivos de build Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.orlandodev.prestamos',
   appName: 'PrestamosApp',
-  webDir: 'dist',
+  webDir: '.output/public',
   server: {
     url: "http://192.168.1.8:3000",
     cleartext: true
